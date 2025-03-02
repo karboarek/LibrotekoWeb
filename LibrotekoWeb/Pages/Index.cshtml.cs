2d0d9cfafc0cf40fee73502a47070aec71bebf56
mail <mail@KARBOAREK>
2025-03-02 17:26:54 +0100

    Add project files.

---
+++ LibrotekoWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Localization;
+using Microsoft.AspNetCore.Http;
+using LibrotekoWeb;
+
+public class IndexModel : PageModel
+{  
+
+      
+    private readonly IStringLocalizer<SharedResources> _localizer;
+
+    public string WelcomeMessage { get; set; }
+
+    public IndexModel(IStringLocalizer<SharedResources> localizer)
+    {
+        _localizer = localizer;
+    }
+
+    public void OnGet()
+    {
+        WelcomeMessage = _localizer["WelcomeMessage"];
+    }
+
+    public IActionResult OnPostSetLanguage(string culture)
+    {
+        Response.Cookies.Append(
+            CookieRequestCultureProvider.DefaultCookieName,
+            CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+            new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) });
+
+        return RedirectToPage(); // Odświeżenie strony, aby nowy język się załadował
+    }
+  
+}
+
+
---
+++ LibrotekoWeb/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
+using System.Globalization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Dodaj obsługę lokalizacji
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+// Lista obsługiwanych języków
+var supportedCultures = new[]
+{
+    new CultureInfo("en"),
+    new CultureInfo("pl"),
+    new CultureInfo("da")
+};
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    options.DefaultRequestCulture = new RequestCulture("en");
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+});
+
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+var locOptions = app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value;
+app.UseRequestLocalization(locOptions);
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapRazorPages();
+
+app.Run();
+
+
+
+
